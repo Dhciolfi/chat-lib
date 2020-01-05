fddf2b44e15eca858d0b9f1ca07681c0f55f55d2
Dhciolfi <daniel.ciolfi@gmail.com>
2020-01-05 12:58:29 -0300

    4 - Exibindo a Lista de Mensagens

--- chat_screen.dart
+++ chat_screen.dart
@@ -38,7 +38,38 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text('Olá'),
         elevation: 0,
       ),
-      body: TextComposer(_sendMessage),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('messages').snapshots(),
+              builder: (context, snapshot){
+                switch(snapshot.connectionState){
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    List<DocumentSnapshot> documents =
+                        snapshot.data.documents.reversed.toList();
+
+                    return ListView.builder(
+                      itemCount: documents.length,
+                      reverse: true,
+                      itemBuilder: (context, index){
+                        return ListTile(
+                          title: Text(documents[index].data['text']),
+                        );
+                      }
+                    );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage),
+        ],
+      ),
     );
   }
 }
